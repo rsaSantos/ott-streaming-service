6561b992fc69533fbab9720140c97e37b45c3b69
rsaSantos <rs.elsaint@gmail.com>
2022-11-17 22:37:22 +0000

    flood packet class

--- onode/src/main/java/org/onode/FloodPacket.java
+++ onode/src/main/java/org/onode/FloodPacket.java
@@ -1,2 +1,40 @@
-package org.onode;public class ControlPacket {
+package org.onode;
+
+import org.exceptions.PacketFormatException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FloodPacket
+{
+    public static final String ID = "1";
+
+    private final int jumps;
+    private final long timestamp;
+    private final List<String> ips;
+
+    /**
+     * Here we assume the packet has ID=1
+     * The payload format is: 1;14;timestamp;ip1,ip2,ip3,
+     *
+     * TODO: Throw error of format!
+     *
+     * */
+    public FloodPacket(String payload) throws PacketFormatException
+    {
+        String[] splited = payload.split(";");
+        if(splited[0].equals(FloodPacket.ID) && splited.length == 4)
+        {
+            this.jumps = Integer.parseInt(splited[1]);
+            this.timestamp = Long.parseLong(splited[2]);
+            String[] ips = splited[3].split(",");
+            this.ips = new ArrayList<>();
+            this.ips.addAll(Arrays.asList(ips));
+        }
+        else
+            throw new PacketFormatException("Wrong packet ID!");
+    }
+
+    // TODO: Getters as needed.
 }
